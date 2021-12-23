62f405d83a051b0ada3a3f4e8fe00ca8f0103bca
jgzhang3397 <jgzhang1998@gmail.com>
2021-12-23 23:41:38 +0100

    commit const

---
+++ test_1/Demo01Const.java
@@ -0,0 +1,10 @@
+public class Demo01Const
+{
+    //Was ist Const?
+    //1. string const : "abc", "123" ,"Hello"
+    //2. int const: 123, 200, 0, -29
+    //3. float const: 2.5, -3.13, 0.0
+    //4. char const: 'a', 'v', '9', '\'
+    //5. bool const: true, false
+    //6. null const: null
+}
