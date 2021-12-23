17682d770a8c00d9c60fc44bb478007e1841e67c
jgzhang3397 <jgzhang1998@gmail.com>
2021-12-23 23:34:49 +0100

    HelloWorld

---
+++ test_1/HelloWorld.java
@@ -0,0 +1,18 @@
+//public class + 类名，类名==文件名！！
+public class HelloWorld
+{
+    //main，der Punkt zum Starten
+    public static void main(String[] args)
+    {
+            System.out.println("Hello World!");
+    }
+}
+
+//======keyword=======
+//  1.完全小写的
+//  2.有特殊颜色
+
+//标示符
+//自己定义的，类的名字，方法的名字和变量ming
+//类名：大驼峰式 z.B. HelloWorld
+//方法名和变量名：小驼峰式 z.B. testOne
